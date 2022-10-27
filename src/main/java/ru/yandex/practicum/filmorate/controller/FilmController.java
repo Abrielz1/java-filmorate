158b97316f590e3ffc13debdfec54c20660cd268
Abriel <ravenz1@yandex.ru>
2022-10-27 14:10:35 +0300

    mileStone 0.0.3.6: fixed

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequestMapping("/films")
-
 public class FilmController {
     private static final LocalDate DATE = LocalDate.of(1895, 12, 28);
     private int filmId = 1;
@@ -27,7 +26,6 @@ public class FilmController {
 
     @PostMapping
     public Film create(@Valid @RequestBody Film film) {
-
         validate(film);
         checkFilm(film);
         film.setId(filmId++);
@@ -38,7 +36,6 @@ public class FilmController {
 
     @PutMapping
     public Film put(@Valid @RequestBody Film film) {
-
         validate(film);
         if (!films.containsKey(film.getId())) throw new ValidationException("Такого фильма нет");
         films.remove(film.getId());
@@ -49,10 +46,11 @@ public class FilmController {
     }
 
     private void validate(@Valid @RequestBody Film film) {
-
-        if (film.getReleaseDate().isBefore(DATE) || film.getDuration() < 0)
+          if (film.getReleaseDate().isBefore(DATE) || film.getDuration() < 0) {
+            log.warn("film.getReleaseDate film release date: '{}'/n film.getDuration film duration: {}", film.getReleaseDate(), film.getDuration());
             throw new ValidationException("В то время кино еще не было или продолжительность неверная");
-        if (film.getDescription().length() > 200)
+        } if (film.getDescription().length() > 200)
+            log.warn("film.getDescription описание: '{}'", film.getDescription());
             throw new ValidationException("Слишком длинное описание");
     }
 
@@ -60,6 +58,7 @@ public class FilmController {
         Collection<Film> filmCollection = films.values();
         for (Film fl : filmCollection) {
             if (film.getName().equals(fl.getName()) && film.getReleaseDate().equals(fl.getReleaseDate()))
+                log.warn("film film: '{}'/n fl film: {}", film, fl);
                 throw new ValidationException("Такой фильм уже есть");
         }
     }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -14,7 +14,6 @@ import java.util.Map;
 @RestController
 @Slf4j
 @RequestMapping("/users")
-
 public class UserController {
     private int userId = 1;
     private final Map<Integer, User> users = new HashMap<>();
@@ -26,7 +25,6 @@ public class UserController {
 
     @PostMapping
     public User create(@Valid @RequestBody User user) {
-
         validate(user);
         checkUsers(user);
         user.setId(userId++);
@@ -37,7 +35,6 @@ public class UserController {
 
     @PutMapping
     public User put(@Valid @RequestBody User user) {
-
         validate(user);
         if (!users.containsKey(user.getId()))
             throw new ValidationException("Пользователя не существует, необходима регистрация нового пользователя");
@@ -49,20 +46,21 @@ public class UserController {
     }
 
     private void validate(@Valid @RequestBody User user) {
-
         if (user.getLogin().contains(" ")) {
+            log.warn("Логин юзера '{}'", user.getLogin());
             throw new ValidationException("Логин не может быть пустым или содержать пробелы");
         }
         if (user.getName() == null || user.getName().isBlank()) user.setName(user.getLogin());
         if (user.getBirthday().isAfter(LocalDate.now()))
+            log.warn("Возраст '{}'", user.getBirthday());
             throw new ValidationException("Вход до 13 лет запрещён!");
     }
 
     private void checkUsers(@RequestBody User user) {
-
         Collection<User> userCollection = users.values();
         for (User us : userCollection) {
             if (user.getLogin().equals(us.getLogin()) || user.getEmail().equals(us.getEmail()) ) {
+                log.warn("user e-mail: '{}'/n us email: {}", user, us);
                 throw new ValidationException("Пользователь с таким email или login уже существует");
             }
         }
