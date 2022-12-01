27c7a0cb1e83b3bd0db64b594351696d51d621aa
Abriel <106871954+Abrielz1@users.noreply.github.com>
2022-12-01 23:19:29 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,20 +1,16 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.RequiredArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.validation.constraints.*;
 import java.time.LocalDate;
-import java.util.HashSet;
-import java.util.Set;
+import lombok.*;
 
-@RequiredArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
 public class User {
 
-    private Set<Integer> friends = new HashSet<>();
 
     @PositiveOrZero
     private int id;
@@ -28,6 +24,4 @@ public class User {
     @NotNull(message = "Не указана дата рождения")
     @PastOrPresent(message = "Некорректная дата рождения")
     private LocalDate birthday;
-    private StatusRelation statusRelation;
-
 }
